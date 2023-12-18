5aad494fd87f3ee8b39036be1cf13f9a609bc2e1
Prathamesh Kulkarni <prathameshkulkarni952@gmail.com>
2023-12-18 21:29:41 +0530

    changed prisma client

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,19 @@
 import { PrismaClient } from "@prisma/client";
-import { env } from "../../env";
+import "server-only";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+declare global {
+  // eslint-disable-next-line no-var, no-unused-vars
+  var cachedPrisma: PrismaClient;
+}
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
 
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+export const db = prisma;
