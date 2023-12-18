3f9b73ea9c79d03ba6f21a56dc6d980d374cc68e
Prathamesh Kulkarni <prathameshkulkarni952@gmail.com>
2023-12-18 22:48:37 +0530

    changed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,19 +9,19 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
   }
 
-  if (
-    req.nextUrl.pathname.startsWith("/dashboard/patient") &&
-    token.role === "doctor"
-  ) {
-    return NextResponse.rewrite(new URL("/dashboard/doctor", req.url));
-  }
-
-  if (
-    req.nextUrl.pathname.startsWith("/dashboard/doctor") &&
-    token.role !== "doctor"
-  ) {
-    return NextResponse.rewrite(new URL("/dashboard/patient", req.url));
-  }
+  // if (
+  //   req.nextUrl.pathname.startsWith("/dashboard/patient") &&
+  //   token.role === "doctor"
+  // ) {
+  //   return NextResponse.rewrite(new URL("/dashboard/doctor", req.url));
+  // }
+  //
+  // if (
+  //   req.nextUrl.pathname.startsWith("/dashboard/doctor") &&
+  //   token.role !== "doctor"
+  // ) {
+  //   return NextResponse.rewrite(new URL("/dashboard/patient", req.url));
+  // }
 }
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
