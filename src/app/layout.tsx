8c90e36b7902ff7ce81f4cdf971d21c0f3541a4d
Prathamesh Kulkarni <prathameshkulkarni952@gmail.com>
2023-12-22 20:55:05 +0530

    added vercel speed insigths

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import Providers from "@/components/Providers";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
           {children}
         </Providers>
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
