6e8f52dcd73d5f4ed1cac974e89cbcda310f40af
Prathamesh Kulkarni <prathameshkulkarni952@gmail.com>
2024-01-17 00:33:50 +0530

    feat: imporove background

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,28 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./ui/Button";
+
 async function Hero() {
   return (
-    <main className="flex flex-col justify-between lg:px-36 lg:mt-20">
-      <section className="flex flex-col lg:flex-row justify-between">
-        <div className="flex items-start flex-col justify-end">
-          <h1 className="text-6xl font-bold">Empowering Healthcare</h1>
-          <p>Innovative Medical Records Management for a Healthier Tomorrow</p>
+    <main className="flex flex-col justify-center lg:px-36 items-center min-h-screen">
+      <section className="flex flex-col justify-between gap-8">
+        <div className="flex flex-col">
+          <h1 className="flex justify-center lg:text-6xl font-bold text-2xl">
+            Empowering Healthcare
+          </h1>
+          <p className="flex text-center items-center justify-center">
+            Innovative Medical Records Management for a Healthier Tomorrow
+          </p>
         </div>
-        <div></div>
+        <Link
+          href="/sign-in"
+          className={cn(
+            buttonVariants({ variant: "default" }),
+            "rounded-lg m-auto",
+          )}
+        >
+          Get Started
+        </Link>
       </section>
     </main>
   );
--- src/components/MainNavBar.tsx
+++ src/components/MainNavBar.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import {
   Drawer,
-  DrawerClose,
   DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
@@ -13,17 +10,19 @@ import { Button } from "./ui/Button";
 
 function MainNavBar() {
   return (
-    <nav className="lg:bg-transparent flex flex-row justify-between lg:px-36 lg:pt-14 p-5">
+    <nav className="lg:bg-transparent flex flex-row justify-between lg:px-36 lg:pt-14 p-5 fixed w-[100%]">
       <div className="lg:text-black lg:text-xl font-bold text-md">
         MedOrbX
         <sup className="rounded-full p-1 bg-purple-500 font-normal text-sm text-muted">
           beta
         </sup>
       </div>
-      <div className="lg:hidden bg-black text-white">
+      <div className="lg:hidden">
         <Drawer>
           <DrawerTrigger asChild>
-            <Button variant="outline">Open Drawer</Button>
+            <Button variant="default" className="rounded-full">
+              Menu
+            </Button>
           </DrawerTrigger>
           <DrawerContent className="text-white bg-black">
             <div className="mx-auto w-full max-w-sm">
