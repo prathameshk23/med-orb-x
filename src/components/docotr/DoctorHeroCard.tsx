b2542d5ce4525470e4cc683ae531c100b48f7d19
Prathamesh Kulkarni <prathameshkulkarni952@gmail.com>
2024-01-28 18:16:36 +0530

    fix: responsive issue on xl screen

--- src/components/docotr/DoctorHeroCard.tsx
+++ src/components/docotr/DoctorHeroCard.tsx
@@ -8,7 +8,7 @@ async function DoctorHeroCard() {
   return (
     <div className="rounded-3xl lg:my-4 mt-36 flex lg:flex-row flex-col justify-between items-center bg-sky-400 lg:w-4/6 w-fit h-[35vh] min-h-fit">
       <div className="flex justify-end items-end p-12">
-        <p className="text-xl lg:text-2xl xl:text-4xl font-bold break-all">
+        <p className="text-xl xl:text-2xl 2xl:text-4xl font-bold break-all">
           Hello, Dr. {session?.user.name?.split(" ")[0]}
         </p>
       </div>
@@ -17,7 +17,7 @@ async function DoctorHeroCard() {
         alt="Image of doctor"
         width={400}
         height={400}
-        className="lg:mt-9 size-[12rem] xl:size-[25rem] lg:size-[14rem]"
+        className="lg:mt-9 size-[12rem] 2xl:size-[25rem] xl:size-[14rem]"
       />
     </div>
   );
--- src/components/docotr/DoctorProfileCard.tsx
+++ src/components/docotr/DoctorProfileCard.tsx
@@ -6,8 +6,8 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 async function DoctorProfileCard() {
   const session = await getServerSession(authOptions);
   return (
-    <div className="hidden min-h-[200px] lg:w-4/12 w-full bg-indigo-800 mx-4 flex-wrap my-4 h-[35vh] rounded-3xl p-4 lg:flex flex-row justify-start items-center gap-2">
-      <Avatar className="xl:h-24 xl:w-24 w-16 h-16">
+    <div className="hidden min-h-[200px] xl:w-4/12 w-full bg-indigo-800 mx-4 flex-wrap my-4 h-[35vh] rounded-3xl p-4 lg:flex flex-row justify-start items-center gap-2">
+      <Avatar className="2xl:h-24 2xl:w-24 w-16 h-16">
         <AvatarImage
           src={session?.user.image as string}
           alt="Profile picture of doctor"
@@ -15,10 +15,10 @@ async function DoctorProfileCard() {
         <AvatarFallback>Profice picture</AvatarFallback>
       </Avatar>
       <div className="flex flex-col justify-center items-start gap-1 break-all">
-        <p className="text-lg xl:text-2xl font-bold text-white">
+        <p className="text-lg 2xl:text-2xl font-bold text-white">
           Dr. {session?.user.name?.split(" ")[0]}
         </p>
-        <div className="text-xs xl:text-lg">
+        <div className="text-xs 2xl:text-lg">
           <p>@{session?.user.username}</p>
           <p>{session?.user.email}</p>
           <p>{session?.user.address}</p>
